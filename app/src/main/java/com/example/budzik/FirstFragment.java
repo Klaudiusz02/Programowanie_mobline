797b3e6ceb2e07159d903715d98241fd2bd01ed1
Klaudiusz02 <unlocked01@o2.pl>
2023-11-06 22:03:47 +0100

    Zapisywanie alarmów w SharedPreferences

--- app/src/main/java/com/example/budzik/FirstFragment.java
+++ app/src/main/java/com/example/budzik/FirstFragment.java
@@ -1,5 +1,7 @@
 package com.example.budzik;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
@@ -9,6 +11,7 @@ import android.view.ViewGroup;
 import android.app.TimePickerDialog;
 
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -26,10 +29,14 @@ import com.example.budzik.databinding.FragmentFirstBinding;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.util.Calendar;
+import java.util.Map;
 
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+
+    private SharedPreferences sharedPreferences;
+    private LinearLayout containerLayout;
 
     @Override
     public View onCreateView(
@@ -38,9 +45,11 @@ public class FirstFragment extends Fragment {
     ) {
         View view = inflater.inflate(R.layout.fragment_first, container, false);
 
-        FloatingActionButton fab = view.findViewById(R.id.fab);
-        LinearLayout containerLayout = view.findViewById(R.id.container);
+        containerLayout = view.findViewById(R.id.container);
 
+        sharedPreferences = requireContext().getSharedPreferences("Alarms", Context.MODE_PRIVATE);
+
+        FloatingActionButton fab = view.findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -57,60 +66,7 @@ public class FirstFragment extends Fragment {
                         new TimePickerDialog.OnTimeSetListener() {
                             @Override
                             public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-                                CardView cardView = new CardView(requireContext());
-                                int heightInPixels = 300;
-                                int leftMargin = 16;
-                                int topMargin = 8;
-                                int rightMargin = 16;
-                                int bottomMargin = 8;
-                                float textSizeInSp = 30;
-
-                                LinearLayout.LayoutParams cardLayoutParams = new LinearLayout.LayoutParams(
-                                        LinearLayout.LayoutParams.MATCH_PARENT,
-                                        heightInPixels
-                                );
-
-                                cardLayoutParams.setMargins(leftMargin, topMargin, rightMargin, bottomMargin);
-                                cardView.setLayoutParams(cardLayoutParams);
-                                cardView.setCardElevation(8);
-                                cardView.setRadius(16);
-
-                                LinearLayout innerLayout = new LinearLayout(requireContext());
-                                innerLayout.setOrientation(LinearLayout.VERTICAL);
-                                innerLayout.setPadding(16,16,16,16);
-
-                                TextView textView = new TextView(requireContext());
-                                String formattedTime = String.format("%02d:%02d", selectedHour, selectedMinute);
-                                textView.setText(formattedTime);
-                                textView.setTextSize(18);
-                                textView.setPadding(16, 16, 16, 16);
-                                textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, textSizeInSp);
-
-                                Switch switchButton = new Switch(requireContext());
-                                switchButton.setChecked(true);
-                                switchButton.setThumbResource(R.drawable.custom_thumb);
-                                switchButton.setTrackResource(R.drawable.custom_track);
-
-                                Button deleteButton = new Button(requireContext());
-                                deleteButton.setText("Usuń");
-                                deleteButton.setLayoutParams(new LinearLayout.LayoutParams(
-                                        LinearLayout.LayoutParams.WRAP_CONTENT,
-                                        LinearLayout.LayoutParams.WRAP_CONTENT
-                                ));
-                                deleteButton.setOnClickListener(new View.OnClickListener() {
-                                    @Override
-                                    public void onClick(View view) {
-                                        containerLayout.removeView(cardView);
-                                    }
-                                });
-
-                                innerLayout.addView(textView);
-                                innerLayout.addView(switchButton);
-                                innerLayout.addView(deleteButton);
-
-                                cardView.addView(innerLayout);
-
-                                containerLayout.addView(cardView);
+                                addAlarm(selectedHour, selectedMinute);
                             }
                         },
                         hour, minute, true);
@@ -119,14 +75,95 @@ public class FirstFragment extends Fragment {
             }
         });
 
-        return view;
+        restoreAlarms();
 
+        return view;
     }
 
+    private void addAlarm(int hour, int minute) {
+        String formattedTime = String.format("%02d:%02d", hour, minute);
+        String alarmState = "Włączony";
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(formattedTime, alarmState);
+        editor.apply();
+
+        createAlarmCard(formattedTime, alarmState);
+    }
+
+    private void restoreAlarms() {
+        Map<String, ?> alarmsMap = sharedPreferences.getAll();
+
+        for (Map.Entry<String, ?> entry : alarmsMap.entrySet()) {
+            String time = entry.getKey();
+            String state = entry.getValue().toString();
+            createAlarmCard(time, state);
+        }
+    }
+
+    private void createAlarmCard(String time, String state) {
+        CardView cardView = new CardView(requireContext());
+        int heightInPixels = 300;
+        int leftMargin = 16;
+        int topMargin = 8;
+        int rightMargin = 16;
+        int bottomMargin = 8;
+        float textSizeInSp = 30;
+
+        LinearLayout.LayoutParams cardLayoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                heightInPixels
+        );
+
+        cardLayoutParams.setMargins(leftMargin, topMargin, rightMargin, bottomMargin);
+        cardView.setLayoutParams(cardLayoutParams);
+        cardView.setCardElevation(8);
+        cardView.setRadius(16);
+
+        LinearLayout innerLayout = new LinearLayout(requireContext());
+        innerLayout.setOrientation(LinearLayout.VERTICAL);
+        innerLayout.setPadding(16, 16, 16, 16);
+
+        TextView textView = new TextView(requireContext());
+        textView.setText(time);
+        textView.setTextSize(18);
+        textView.setPadding(16, 16, 16, 16);
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, textSizeInSp);
+
+
+        Switch switchButton = new Switch(requireContext());
+        switchButton.setChecked(state.equals("Włączony"));
+
+        switchButton.setThumbResource(R.drawable.custom_thumb);
+        switchButton.setTrackResource(R.drawable.custom_track);
+
+        Button deleteButton = new Button(requireContext());
+        deleteButton.setText("Usuń");
+        deleteButton.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        ));
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                containerLayout.removeView(cardView);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.remove(time);
+                editor.apply();
+            }
+        });
+
+        innerLayout.addView(textView);
+        innerLayout.addView(switchButton);
+        innerLayout.addView(deleteButton);
+
+        cardView.addView(innerLayout);
+
+        containerLayout.addView(cardView);
+    }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
 
     }
 
